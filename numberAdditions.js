c0cdb94d0e4447b910d1b60da56b1ffdef269802
tanmoydas9166 <tanmoydas9166@gmail.com>
2024-10-10 15:30:23 +0000

    Support different delimiters

--- numberAdditions.js
+++ numberAdditions.js
@@ -2,10 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.add = add;
 function add(numbers) {
-    var allNumbers = numbers.split(/[\n,]/).map(function (n) { return Number(n.trim()); });
+    var delimeter = ',';
+    if (numbers.substring(0, 2) === '//') {
+        delimeter = numbers[2];
+        numbers = numbers.substring(3);
+    }
+    var regex = new RegExp("[\n".concat(delimeter, "]"));
+    var allNumbers = numbers.split(regex).map(function (n) { return Number(n.trim()); });
     return allNumbers.reduce(function (pv, cv) { return pv + cv; }, 0);
 }
 console.log(add('1,5'));
 console.log(add('1'));
 console.log(add(''));
 console.log(add('1\n2,3'));
+console.log(add('//+\n1+2'));
--- numberAdditions.ts
+++ numberAdditions.ts
@@ -1,5 +1,11 @@
 export function add(numbers: string) {
-    const allNumbers = numbers.split(/[\n,]/).map(n => Number(n.trim()));
+    let delimeter = ',';
+    if (numbers.substring(0, 2) === '//') {
+        delimeter = numbers[2];
+        numbers = numbers.substring(3)
+    }
+    const regex = new RegExp(`[\n${delimeter}]`)
+    const allNumbers = numbers.split(regex).map(n => Number(n.trim()));
     return allNumbers.reduce((pv, cv) => pv+cv, 0)
 }
 
@@ -7,3 +13,4 @@ console.log(add('1,5'))
 console.log(add('1'))
 console.log(add(''))
 console.log(add('1\n2,3'))
+console.log(add('//+\n1+2'))
