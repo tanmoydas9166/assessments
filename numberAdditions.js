6c1d6a8a1c0484231ea0c95d0aa58f7b3e44fa02
tanmoydas9166 <tanmoydas9166@gmail.com>
2024-10-10 15:16:27 +0000

    Allow the add method to handle new lines between numbers

--- numberAdditions.js
+++ numberAdditions.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.add = add;
 function add(numbers) {
-    var allNumbers = numbers.split(',').map(function (n) { return Number(n.trim()); });
+    var allNumbers = numbers.split(/[\n,]/).map(function (n) { return Number(n.trim()); });
     return allNumbers.reduce(function (pv, cv) { return pv + cv; }, 0);
 }
 console.log(add('1,5'));
 console.log(add('1'));
 console.log(add(''));
+console.log(add('1\n2,3'));
--- numberAdditions.ts
+++ numberAdditions.ts
@@ -1,8 +1,9 @@
 export function add(numbers: string) {
-    const allNumbers = numbers.split(',').map(n => Number(n.trim()));
+    const allNumbers = numbers.split(/[\n,]/).map(n => Number(n.trim()));
     return allNumbers.reduce((pv, cv) => pv+cv, 0)
 }
 
 console.log(add('1,5'))
 console.log(add('1'))
 console.log(add(''))
+console.log(add('1\n2,3'))
